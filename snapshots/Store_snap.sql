8f874d0ef9353374b857b89db0c728798a80ca5f
Jayant Pandey <jayant.pandey@accordion.com>
2026-07-20 16:29:33 +0000

    working

--- snapshots/Store_snap.sql
+++ snapshots/Store_snap.sql
@@ -1,50 +1,26 @@
-
 {% snapshot Store_snap %}
- 
 {{
     config(
-      unique_key='store_id',
+      unique_key='store_id'
     )
 }}
  
-with unwrapped as (
+with flattened as (
     select
-        st.value                        as store_json,
         file_last_modified,
-        _batch_id,
         _loaded_at,
-        _source_file
+        _batch_id,
+        _source_file,
+        sto.value:store_id::varchar as store_id,
+        sto.value as sto_json
+
     from {{ ref('Bronze_store_data') }},
-         lateral flatten(input => raw_json_payload:stores_data) st
-),
- 
-extracted as (
-    select
-        store_json:store_id::string       as store_id,
-        store_json                         as sto_json,
-        file_last_modified,
-        _batch_id,
-        _loaded_at,
-        _source_file
-    from unwrapped
-),
- 
-cleaned as (
-    select
-        *
-    from extracted
+    lateral flatten(input => raw_json_payload:stores_data) sto
 )
  
-select *
-from cleaned
-where store_id is not null
-  and store_id != ''
-qualify row_number() over (
-    partition by store_id
-    order by file_last_modified desc, _loaded_at desc
-) = 1
+select * 
+from flattened
  
 {% endsnapshot %}
- 
  
  
